0a7469a350f74fe3222442011e50a80800acc2ca
Dante <dante.collier.2022@student.lhschools.org>
2019-02-11 13:36:05 -0500

    First word + second word

---
+++ src/com/LickingHeights/MathTester.java
@@ -0,0 +1,24 @@
+package com.LickingHeights;
+
+public class MathTester {
+
+        public static void main(String[] args) {
+            String firstWord;
+            String secondWord;
+            int firstNumber, secondNumber;
+
+            firstWord = "hot";
+            secondWord = "dog";
+            firstNumber = 2;
+            secondNumber = 4;
+
+            System.out.println(firstWord+secondWord);
+            System.out.println(firstNumber+secondNumber);
+
+
+        }
+
+
+
+
+}
